d45329615af60cf73cef8cc4a2a5634552baec06
mkihl98 <martinki@hiof.no>
2024-04-25 13:48:18 +0200

    Installert sanity-klient

---
+++ backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'q1tnpr3t',
+    dataset: 'production'
+  }
+})
---
+++ backend/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'prosjekt',
+
+  projectId: 'q1tnpr3t',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
